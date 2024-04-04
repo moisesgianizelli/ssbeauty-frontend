5b498a090b4d3cb46c6bf1f84090dccfdd9d38bf
Moises Munaldi <ork.mgm@gmail.com>
2024-04-04 20:41:33 +0100

    Advances for my main page

---
+++ src/layouts/HomePage/ExploreTopOptions.tsx
@@ -0,0 +1,19 @@
+export const ExploreTopOptions = () => {
+  return (
+    <div className="p-5 mb-4 bg-dark header">
+      <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+        <div>
+          <h1 className="display-5 fw-bold">Explore top options</h1>
+          <p className="col-md-8 fs-4">Which options you want to buy us</p>
+          <a
+            type="button"
+            className="btn main-color btn-lg text-white"
+            href="#"
+          >
+            Explore options
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
