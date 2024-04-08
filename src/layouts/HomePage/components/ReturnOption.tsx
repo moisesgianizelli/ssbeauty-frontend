b13b60c6272b4c5e032917c9c0acd21f26d17f4e
Moises Munaldi <ork.mgm@gmail.com>
2024-04-08 21:10:12 +0100

    Adapting to the actual wedding project

--- src/layouts/HomePage/components/ReturnOption.tsx
+++ src/layouts/HomePage/components/ReturnOption.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export const ReturnOption = () => {
   return (
     <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
       <div className="text-center">
         <img
-          src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+          src={require("./../../../Images/BooksImages/parachute.jpg")}
           width="151"
           height="233"
           alt="book"
         />
-        <h6 className="mt-2">Option</h6>
+        <h6 className="mt-2">Parachute</h6>
         <p>Casorio</p>
         <a className="btn main-color text-white" href="#">
           Choose
