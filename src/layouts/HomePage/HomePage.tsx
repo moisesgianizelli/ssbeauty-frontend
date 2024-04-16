6933cdcc6312ee3238f0a52ab4fef3514913e8e8
Moises Munaldi <ork.mgm@gmail.com>
2024-04-16 18:34:57 +0100

    Advances for gallery

--- src/layouts/HomePage/HomePage.tsx
+++ src/layouts/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
 import { Booking } from "./components/Booking";
 import { ExploreTopOptions } from "./components/ExploreTopOptions";
+import { Gallery } from "./components/Gallery";
 import { Heros } from "./components/Heros";
 
 export const HomePage = () => {
   return (
     <>
       <ExploreTopOptions />
-      <Booking />
       <Heros />
     </>
   );
--- src/layouts/HomePage/components/Gallery.tsx
+++ src/layouts/HomePage/components/Gallery.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import { slides } from "../../../Images/GalleryImages/data";
+import "yet-another-react-lightbox/styles.css";
+import {
+  Captions,
+  Fullscreen,
+  Thumbnails,
+} from "yet-another-react-lightbox/plugins";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import ImagesGallery from "./ImagesGallery";
 
 export const Gallery = () => {
-  return <div></div>;
+  const [index, setIndex] = useState<number>(-1);
+  return (
+    <>
+      <ImagesGallery
+        data={slides}
+        onClick={(currentIndex) => setIndex(currentIndex)}
+      />
+
+      <Lightbox
+        plugins={[Captions, Fullscreen, Thumbnails]}
+        index={index}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+        slides={slides}
+      />
+    </>
+  );
 };
