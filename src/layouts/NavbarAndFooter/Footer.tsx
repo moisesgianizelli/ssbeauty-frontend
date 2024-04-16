469dd1aef409a10fa050b5f3cae88aff7e8e45d1
moisesgianizelli <ork.mgm@gmail.com>
2024-04-16 21:19:01 +0100

    Advances for gallery

--- src/layouts/NavbarAndFooter/Footer.tsx
+++ src/layouts/NavbarAndFooter/Footer.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export const Footer = () => {
   return (
     <div className="main-color">
@@ -5,14 +7,14 @@ export const Footer = () => {
         <p className="col-md-4 mb-0 text-white">SSBeauty App, Inc</p>
         <ul className="nav navbar-dark col-md-4 justify-content-end">
           <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-white">
+            <NavLink to="/home" className="nav-link px-2 text-white">
               Home
-            </a>
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-white">
+            <NavLink to="/gallery" className="nav-link px-2 text-white">
               Galeria
-            </a>
+            </NavLink>
           </li>
         </ul>
       </footer>
