245911b38912816b5042b01fa97803720de86d28
moisesgianizelli <ork.mgm@gmail.com>
2024-05-12 14:10:35 +0100

    advances for main page

--- src/layouts/HomePage/components/Heros.tsx
+++ src/layouts/HomePage/components/Heros.tsx
@@ -4,7 +4,7 @@ export const Heros = () => {
   return (
     <div>
       <div className="d-none d-lg-block">
-        <div className="row g-0 mt-5">
+        <div className="row g-0">
           <div className="col-sm-6 col-md-6">
             <div className="col-image-left"></div>
           </div>
@@ -24,7 +24,7 @@ export const Heros = () => {
         <div className="row g-0">
           <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
             <div className="ml-2">
-              <h1>Conheca outros servicos</h1>
+              <h1>Micropigmentação</h1>
               <p className="lead">
                 Sempre estamos nos atualizando e postando novas imagens
               </p>
@@ -38,6 +38,23 @@ export const Heros = () => {
           </div>
           <div className="col-sm-6 col-md-6">
             <div className="col-image-right"></div>
+          </div>
+        </div>
+        <div className="row g-0">
+          <div className="col-sm-6 col-md-6">
+            <div className="col-image-left2"></div>
+          </div>
+          <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+            <div className="ml-2">
+              <h1>Sombrancelha</h1>
+              <p className="lead">Procurando servico de beleza</p>
+              <NavLink
+                className="btn main-color btn-lg text-white"
+                to="/booking"
+              >
+                Agende aqui
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
