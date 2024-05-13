7989afd38930863ed36f853f0940098d93ddb88a
moisesgianizelli <ork.mgm@gmail.com>
2024-05-13 22:31:45 +0100

    small adjustment

--- src/layouts/HomePage/components/Agendamento.tsx
+++ src/layouts/HomePage/components/Agendamento.tsx
@@ -4,7 +4,7 @@ import { InlineWidget } from "react-calendly";
 export const Agendamento = () => {
   return (
     <div className="App">
-      <InlineWidget url="https://calendly.com/ork-mgm" />
+      <InlineWidget url="https://calendly.com/ork-mgm/minha-agenda" />
     </div>
   );
 };
